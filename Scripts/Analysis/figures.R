7765d9656a041e3005e89331c2344d41acfe934d
dswatson <david.watson@oii.ox.ac.uk>
2019-04-10 11:37:32 +0100

    phi_over_time

--- Scripts/Analysis/figures.R
+++ Scripts/Analysis/figures.R
@@ -16,6 +16,24 @@ df[partyId == 1, pId := 'Conservative'
   ][partyId == 2, pId := 'Labour'
   ][partyId == 3, pId := 'Lib Dem']
 
+# PHI over time
+df %>%
+  filter(!is.na(phi), !is.na(weight), year > 1992) %>%
+  group_by(year) %>%
+  summarise(PHI = weighted.mean(phi, weight),
+            SE = sd(phi) / sqrt(length(phi))) %>%
+  rename(Year = year) %>%
+  select(Year, PHI, SE) %>%
+  ggplot(aes(Year, PHI)) + 
+  geom_point() + 
+  geom_smooth(method = 'gam', se = FALSE, formula = y ~ s(x, k = 6)) + 
+  geom_errorbar(aes(ymin = PHI - SE, ymax = PHI + SE), width = 0.25) +
+  labs(title = 'PHI Over Time',
+       y = 'Personal Hostility Index') + 
+  theme_bw() + 
+  theme(plot.title = element_text(hjust = 0.5))
+ggsave('./Figures/phi_over_time.pdf')
+
 # PHI by party over time
 df %>%
   filter(!is.na(phi), !is.na(weight), year > 1992) %>%
@@ -52,7 +70,7 @@ df %>%
         axis.text.x = element_text(angle = 45L, hjust = 1))
 ggsave('./Figures/phi_by_party_static.pdf')
 
-# Distance matrices
+# Trajectories by party
 likes <- fread('./Data/likes.csv')[year > 1992]
 parties <- c('Con', 'Lab', 'LD')
 dm_df <- crossing(Year = likes[, unique(year)],
@@ -69,8 +87,6 @@ dm_df <- dm_df %>%
   mutate(Distance = mu_fn(Year, Evaluated, Evaluator),
          SE = se_fn(Year, Evaluated, Evaluator)) %>%
   as.data.table(.)
-
-# Trajectories by party
 dm_df[Evaluator == 'Con', Evaluator := 'Conservative'
   ][Evaluator == 'Lab', Evaluator := 'Labour'
   ][Evaluator == 'LD', Evaluator := 'Lib Dem'
